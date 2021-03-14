437bf48d80f9aa849b1e59200872b8be89b0e119
alejorminetti <alejorm@gmail.com>
2021-03-14 12:26:21 -0300

    Post list with like button

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(MyApp());
 }
 
+class Post {
+  String body;
+  String author;
+  int likes = 0;
+  bool userLiked = false;
+
+  Post(this.body, this.author);
+
+  void likePost() {
+    this.userLiked = !this.userLiked;
+    if (this.userLiked) {
+      this.likes++;
+    } else {
+      this.likes--;
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -11,7 +29,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tim & CHAMA App',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.brown,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -25,20 +43,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String text = "";
+  List<Post> posts = [];
 
-  void changeText(String text) {
+  void newPost(String text) {
     this.setState(() {
-      this.text = text;
+      posts.add(new Post(text, "CHAMA"));
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Button presses and callbacks')),
+        appBar: AppBar(title: Text('Making a List of posted texts')),
         body: Column(
-          children: [TextInputWidget(this.changeText), Text(this.text)],
+          children: <Widget>[
+            Expanded(
+                // expanded for get space from available free space
+                child: PostList(this.posts)),
+            Expanded(child: TextInputWidget(this.newPost))
+          ],
         ));
   }
 }
@@ -46,7 +69,8 @@ class _MyHomePageState extends State<MyHomePage> {
 class TextInputWidget extends StatefulWidget {
   final Function(String) callback;
 
-  TextInputWidget(this.callback); // {param} makes an optional parameter (param: asd) [] optional unnamed, without any of this: mandatory param
+  TextInputWidget(
+      this.callback); // {param} makes an optional parameter (param: asd) [] optional unnamed, without any of this: mandatory param
 
   @override
   _TextInputWidgetState createState() => _TextInputWidgetState();
@@ -76,9 +100,58 @@ class _TextInputWidgetState extends State<TextInputWidget> {
           suffixIcon: IconButton(
             icon: Icon(Icons.send),
             splashColor: Colors.lightBlue,
-            tooltip: "Post message",
+            tooltip: "Post message to the list",
             onPressed: this.click,
           )),
+    );
+  }
+}
+
+class PostList extends StatefulWidget {
+  final List<Post> listItems;
+
+  PostList(this.listItems);
+
+  @override
+  _PostListState createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+
+void like(Function callback){
+  this.setState(() {
+    callback();
+  });
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (context, index) {
+        var post = this.widget.listItems[index];
+        return Card(
+            child: Row(children: <Widget>[
+          Expanded(
+              child: ListTile(
+            title: Text(post.body),
+            subtitle: Text(post.author),
+          )),
+          Row(
+            children: <Widget>[
+              Container(child: Text(post.likes.toString(),
+              style: TextStyle(fontSize: 20),),
+              padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+              ),
+              IconButton(
+                  icon: Icon(Icons.thumb_up_alt_rounded),
+                  onPressed: () => like(post.likePost),
+                  color: post.userLiked ? Colors.green : Colors.grey,
+                  )
+            ],
+          )
+        ]));
+      },
     );
   }
 }
